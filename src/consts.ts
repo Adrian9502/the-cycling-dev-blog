74b7d6f76e86acd9a3a3eb9a2aeda2ec68e2ce2e
Adrian9502 <bontojohnadrian@gmail.com>
2026-07-14 10:08:59 +0800

    chore: ignore .vercel directory and update site metadata constants

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "The Cycling Dev";
+export const SITE_DESCRIPTION =
+  "Optimizing code and tracking cadence. A software engineer's guide to indoor cycling, telemetry data apps, and working from home.";
